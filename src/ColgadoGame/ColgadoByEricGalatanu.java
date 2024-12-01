9341afd9f1e464d2228e485d6e4fdb220b11ebd5
EricGalatanu <egalatanu.programador@gmail.com>
2024-12-01 23:20:31 +0100

    CommitPrueba
    Primer commit de prueba, solo saldran las variables utilizadas

---
+++ src/ColgadoGame/ColgadoByEricGalatanu.java
@@ -0,0 +1,31 @@
+package ColgadoGame;
+
+import java.util.Scanner;
+
+public class ColgadoByEricGalatanu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Scanner s = new Scanner(System.in);
+
+		String respReglas;
+		String respJugadores;
+		String respJugador1;
+		String respJugador2;
+		String respJugador3;
+		String palabraOriginal = "";
+		String palabraOcultada;
+		int intentosMax = 6;
+		int intentosActuales;
+		int rondas;
+		int rondasJugadas = 0;
+		int puntajeJugador1 = 0;
+		int puntajeJugador2 = 0;
+		
+		
+		
+	}
+
+}
